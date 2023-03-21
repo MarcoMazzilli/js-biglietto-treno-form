ac6ff1d6ac5a6bb938d026923ef7e2efaec8994e
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-03-21 15:04:34 +0100

    [Add] Price Calculator + discount

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,32 @@
-console.log(document);
+//Dati in possesso
+const prezzoAlKm = 0.21;
+const discountUnder = 20;
+const discountOver = 40;
+
+//Intercetto il bottone "submit" e lo assegno ad una variabile
+const submit = document.getElementById('submitInput')
+console.log(submit)
+
+//Al click questo bottone recupera il value degli input e li salva in una var.
+submit.addEventListener('click', function(){
+  const name = document.getElementById("nameInput").value;
+  const distance = document.getElementById('distanceInput').value;
+  const discount = document.getElementById('select').value;
+  console.log(name , distance, discount);
+
+//Calcolo costo biglietto
+let prezzoLordo = distance * prezzoAlKm;
+console.log ( 'Prezzo del biglietto non scontato -->', prezzoLordo);
+
+//Calolo la percentuale di sconto e arrotondo la cifra
+if (discount === "mineronne"){
+  prezzoNetto = prezzoLordo*= 1 -(discountUnder / 100);
+}else if (discount === "over-65"){
+  prezzoNetto = prezzoLordo*= 1 -(discountOver / 100);
+}else {
+  prezzoNetto = prezzoLordo;
+}
+prezzoNetto = prezzoNetto.toFixed(2)
+
+console.log('Prezzo al quale è stato applicato lo sconto -->', prezzoNetto);
+})
